86d7768e7b22710e1b9d9bf5976e8525c0cde165
HumanM00n <mrouy92@gmail.com>
2024-03-27 10:42:20 +0100

    UPDATE

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,21 +3,21 @@
 <html>
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>App Name - @yield('title')</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <title>App Name - @yield('title')</title>
 </head>
 <body>
     @section('sidebar')
-    <nav class="navbar navbar-expand-lg bg-body-tertiary" >
+    <nav class="navbar navbar-expand-lg bg-body-tertiary w-100" >
         <div class="container-fluid">
             <a class="navbar-brand" href="/">Bibliothèque</a>
 
             @guest {{-- SI L'UTILISATEUR N'EST PAS CONNECTE  --}} 
             
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 w-100 d-flex justify-content-end">
                     <li class="nav-item">
-                        {{-- <a class="nav-link" href="/connexion">Connexion</a> --}}
                         <a class="nav-link" href="/connexion">Connexion</a>
                     </li>
                     <li class="nav-item">
@@ -33,10 +33,16 @@
                 @else {{-- SI L'UTILISATEUR EST PAS CONNECTE  --}} 
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0 w-100 d-flex justify-content-end">
                         <li class="nav-item m">
                             <button class="btn btn-light">{{ Auth::user()->prenom }}</button>
                         </li>
+                        @if (Auth::user()->type == "admin")
+                            <li class="nav-item">
+                                <a href="/ajout-livre" class="nav-link">Ajouter un livre</a>
+                            </li>
+                        @endif
+
                         <li class="nav-item">
                             <form action="/logout" method="post">
                                 @csrf
