f72e2244134da3254b64ee1ff131ab329f33b0c0
HumanM00n <mrouy92@gmail.com>
2024-03-27 10:45:35 +0100

    UPDATE

--- app/Http/Controllers/LivresController.php
+++ app/Http/Controllers/LivresController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Livres;
 use App\Models\Auteur;
+use Illuminate\Support\Facades\Auth;
 
 class LivresController extends Controller
 {
@@ -28,6 +29,22 @@ class LivresController extends Controller
         $imgName = time(). '.' . $image->getClientOriginalExtension();
         $image->move(public_path('uploads'), $imgName);
 
+        // Enregistrer le livre
+        $livre = Livres::create([
+            'titre' =>$request->input('titre'),
+            'isbn' => $isbn,
+            'auteur_id' =>$request->input('auteur'),
+            'annee_de_sortie' =>$request->input('date'),
+            'image' => $imgName
+        ]);
 
+        return back()->with('error', 'Livre Ajouté...');
+    }
+
+    // Méthode qui retourne les informations d'un livre
+    public function detailsLivre($id) {
+        $livre = Livres::where('id', $id);
+
+        return view('details', ['livre' => $livre]);
     }
 }
