f8bf81772e615c130a2527bad85991da27b7ff02
HumanM00n <mrouy92@gmail.com>
2024-03-27 10:41:36 +0100

    UPDATE

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,24 @@
 
 @section('content')
 
-    <div class="d-flex m-1">
+    <div class="books">
         @foreach ($Livres as $Livre) 
             <div class="card" style="width: 18rem;">
-                <img src="{{ $Livre->image }}" class="card-img-top">
+                <img src="{{ asset('uploads/'. $Livre->image) }}" class="card-img-top">
                 <div class="card-body">
                     <h5 class="card-title">{{ $Livre->titre }}</h5>
-                    <p class="card-text">{{ $Livre->anne_de_sortie }}</p>
+                    <p class="card-text">{{ $Livre->annee_de_sortie }}</p>
                     <p class="card-text">{{ $Livre->auteur->prenom }}</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="d-flex justify-content-between ">
+                    <a href="{{ route('details', ['id' => $Livre->id]) }}" class="btn btn-primary"><i class="fa-solid fa-eye"></i></a>
+
+                    @Auth
+                        @if (Auth::user()->type == "admin")
+                            <a href="/modifier" class="btn btn-primary"><i class="fa-solid fa-pen"></i></a>
+                        @endif
+                     @endauth
+                </div>
+                
                 </div>
             </div>
         @endforeach
