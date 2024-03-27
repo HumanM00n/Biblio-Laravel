17a181ac814020cee4405abc333e86747311a56f
HumanM00n <mrouy92@gmail.com>
2024-03-27 10:44:09 +0100

    UPDATE

--- app/Http/Controllers/LivresController.php
+++ app/Http/Controllers/LivresController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Livres;
+use App\Models\Auteur;
 
 class LivresController extends Controller
 {
@@ -11,5 +12,22 @@ class LivresController extends Controller
         $Livres = Livres::all();
 
         return view('home' , compact('Livres'));
+    }
+
+    public function index() {
+        $auteurs = Auteur::all();
+        return view('ajoutlivre', ['auteurs' => $auteurs]);
+    }
+
+    public function store(Request $request) {
+        // Generer l'isbn 
+        $isbn = date('YmdHis');
+
+        // Sauvegarder l'image dans le dossier uploads de public 
+        $image = $request->file('photo');
+        $imgName = time(). '.' . $image->getClientOriginalExtension();
+        $image->move(public_path('uploads'), $imgName);
+
+
     }
 }
